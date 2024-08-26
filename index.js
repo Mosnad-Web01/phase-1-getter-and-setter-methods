51e834a2c568c870156e59b6e5a4af1cfbc232ab
Ahmed Nabil <bplahmed28d@gmail.com>
2024-08-26 14:43:19 +0300

    first commit

--- index.js
+++ index.js
@@ -1,1 +1,46 @@
-// Add your Circle class here
+// circle.js
+class Circle {
+  constructor(radius) {
+    this._radius = radius;
+  }
+
+  // Getter for radius
+  get radius() {
+    return this._radius;
+  }
+
+  // Setter for radius
+  set radius(value) {
+    this._radius = value;
+  }
+
+  // Getter for diameter
+  get diameter() {
+    return 2 * this._radius;
+  }
+
+  // Setter for diameter
+  set diameter(value) {
+    this._radius = value / 2;
+  }
+
+  // Getter for circumference
+  get circumference() {
+    return 2 * Math.PI * this._radius;
+  }
+
+  // Setter for circumference
+  set circumference(value) {
+    this._radius = value / (2 * Math.PI);
+  }
+
+  // Getter for area
+  get area() {
+    return Math.PI * this._radius * this._radius;
+  }
+
+  // Setter for area
+  set area(value) {
+    this._radius = Math.sqrt(value / Math.PI);
+  }
+}
